792a315302e3357793950445cefad718e7fd31e3
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-05-13 16:26:10 +0300

    task 01-02

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const KEY = 'feedback-form-state';
+const inputData = {};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+updateForm();
+
+function updateForm() {
+  const savedData = localStorage.getItem(KEY);
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    form.email.value = email;
+    form.message.value = message;
+    inputData.email = email;
+    inputData.message = message;
+  }
+}
+
+function onFormInput(event) {
+  inputData.email = form.elements.email.value;
+  inputData.message = form.elements.message.value;
+  localStorage.setItem(KEY, JSON.stringify(inputData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formDataToSend = new FormData(event.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    inputData[name] = value;
+  });
+
+  event.currentTarget.reset();
+  localStorage.removeItem(KEY);
+
+  console.log(inputData);
+}
