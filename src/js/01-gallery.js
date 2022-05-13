c0b87775deeea0b3e2f4fc56dfdee420a8ce3c67
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-05-13 13:19:16 +0300

    fix homepage

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryImages = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+    return `
+   <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+      style="display:block"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+    </a>
+  </div>
+      `;
+  };
+  
+  const createGalleryContainer = galleryItems.map(createGalleryItem).join('');
+  
+  galleryImages.insertAdjacentHTML('beforeend', createGalleryContainer);
+
+  let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
