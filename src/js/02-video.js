c154cb33346a8e68e3db00597ac4b8d5ebac7eed
natalia-stebel <93938115+natalia-stebel@users.noreply.github.com>
2022-05-13 16:26:23 +0300

    task 01-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY = 'videoplayer-current-time';
+
+const video = new Player('vimeo-player');
+const key = localStorage.getItem(KEY);
+if (key) {
+  video.setCurrentTime(parseFloat(key));
+}
+
+video.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(KEY, data.seconds.toString());
+  }, 1000),
+);
